7cfca6ab805f6b1b0d89bd7184448d10f7d96982
cristph <1174887809@qq.com>
2016-07-07 09:59:49 +0800

    add project
    add project contnent

---
+++ src/lexicalAnalyse/Start.java
@@ -0,0 +1,11 @@
+package lexicalAnalyse;
+
+/**
+ * Created by cristph on 2015/11/18.
+ */
+public class Start {
+    public static void main(String args[]){
+        LexicalAnalysis la=new LexicalAnalysis();
+        la.analyse("program.txt");
+    }
+}
